c3b465aa64f2c3986be04599713f1e714d450550
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-20 20:06:35 +0200

    Update help.py

--- help.py
+++ help.py
@@ -3,6 +3,11 @@ from getpass import getpass
 import os
 import time
 from msvcrt import getch        # for windows users
+from textwrap import wrap
+import pyttsx3
+
+# record of Student ->     Id + '\t' + Name + '\t' + Age + '\t' + Department + '\t' + Level + '\t' + Password + '\t' + [courses] + '\t\n'
+# record of Teahcer ->     Id + '\t' + Name + '\t' + Age + '\t' + Course_name + '\t' + Password + '\t' + [students] + '\t\n'
 
 class bcolors:
     red = '\033[38;5;196m'
@@ -28,24 +33,32 @@ def SetCo(s):
 def PutSp(n, ch):
     print(n * str(ch), end='')
 
-# record of Student ->     Id + '\t' + Name + '\t' + Age + '\t' + Department + '\t' + Level + '\t' + Password + '\t' + [courses] + '\t\n'
-# record of Teahcer ->     Id + '\t' + Name + '\t' + Age + '\t' + Course_name + '\t' + Password + '\t' + [students] + '\t\n'
-
-
 students_ids = PriorityQueue()
 teachers_ids = PriorityQueue()
 
 existance_student_id = [False for i in range(205)]
 existance_teacher_id = [False for i in range(205)]
 
+def sound(sentence):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[1].id)
+    engine.say(sentence)
+    engine.runAndWait()
+
 def press_any():
-    #PJ: Check https://cutt.ly/QHc8Esv
+    print()
+    SetCo('blue')
+    PutSp(30 , ' ')
     print("Press any key to return back...", end = '', flush=True)
+    sound("press any key to continue please")
     getch()
     print()
     
 
 def Directing():
+    PutSp(30 , ' ')
+    SetCo('magenta')
     print('Redicrecting', end='', flush = True)
     for i in range(3):
         print('.', end='', flush = True)
@@ -55,6 +68,9 @@ def Directing():
     print()
 
 def Returning():
+    print()
+    PutSp(30 , ' ')
+    SetCo('red')
     print('Returning', end='', flush = True)
     for i in range(3):
         print('.', end='', flush = True)
@@ -62,7 +78,6 @@ def Returning():
     time.sleep(0.4)
     os.system("cls")
     print()
-
 
 def init_student_ids():
     with open("Student.txt", 'r') as student_file:
@@ -81,3 +96,13 @@ def init_teacher_ids():
         for i in range(1, 205, 1):
             if not existance_teacher_id[i]:
                 teachers_ids.put(i)
+                
+def table(text , w  , m = 40 ):
+    sentence = text
+    width = w
+    PutSp(m , " ")
+    print('+-' + '-' * width + '-+')
+
+    for line in wrap(sentence, width):
+        PutSp(m , " ")
+        print('| {0:^{1}} |'.format(line, width))
