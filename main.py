6d08481e0adf9391df5f3c3c029b2de07b03334a
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-20 20:07:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from msvcrt import putch
 from student import student
 from teacher import teacher
 from admin import admin
 from help import *
-
-# def prints(srtn):
     
 #PutSp(num, 'ch')
 #SetCo('color')
@@ -12,15 +11,21 @@ def main():
     init_student_ids()
     init_teacher_ids()
     choice = 1
+    sound('welcom to every one')
     while choice != 0:
+        SetCo('green')
+        table("Main Menu" , 50 , 25)
+        table("" , 50 , 25)
+        print()
 
-        print('Main Menu\n')
-        print('1- Student')
-        print('2- Teacher')
-        print('3- Admin')
-        print('0- Exist\n')
-        
-
+        table("  1- Student\n" , 20)
+        table("  2- Teacher\n" , 20)
+        table("3- Admin\n" , 20)
+        SetCo('red')
+        table("0- Exit\n" , 20)
+        table("" , 20)  
+        SetCo('gold')
+        PutSp(20 , " ") 
         choice = int(input('Choice: '))
         if choice == 1:
             student()
@@ -29,10 +34,21 @@ def main():
         elif choice == 3:
             admin()
         elif choice != 0:
+            PutSp(20 , ' ')
+            SetCo('red')
+            sound("pleaes Choose a number between 0 and 3")
             print('Choose a number between 0 and 3')
             Directing()
     Returning()
+    SetCo("gold")
+    PutSp(30,' ')
+    print("created by peter turkey,mina magdy ,mohamed ashraf,eman mohamed and eman el-saied")
+    sound("created by peter turkey,mina magdy ,mohamed ashraf,eman mohamed and eman el-saied")
+    SetCo("blue")
+    
+    PutSp(50,' ')
     print('--> Goodbye <--')
+    sound('goodbye')
 
 if __name__ == "__main__":
-    main()
+   main()
