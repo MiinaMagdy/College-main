43c74eac0eb3b845e2ed17c49c0bc257ccda0d8f
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-05-20 20:10:57 +0200

    Update admin.py

--- admin.py
+++ admin.py
@@ -5,35 +5,55 @@ from ctrl_student import ctrl_student
 def admin():
     while 1:
         Directing()
-        print('1- sign in')
-        print('0- return back')
+        SetCo('green')
+        table('1- sign in',20)
+        SetCo('red')
+        table('0- return back',20)
+        table("",20)
+        SetCo('gold')
+        PutSp(30,' ')
         Reg_sign = input('choose: ')
 
+        
         if Reg_sign == '0':
             Returning()
             return
         if Reg_sign != '1':
+            SetCo('red')
             print('Choose a number between 0 and 1')
+            sound("Choose a number between 0 and 1")
             press_any()
             continue
-
         Directing()
-
-        print('Sign In Admin Page\n')
+        SetCo('blue')
+        table('Sign In Admin Page\n' , 50 , 40)
+        table('' , 50 , 40)
+        
+        PutSp(50,' ')
+        SetCo('brown')
         Id = input('Enter ID: ')
+        PutSp(50,' ')
         Password = getpass('Enter Password: ')
         found = 1
         # found = (Id == Password and Id == 'Admin')
         if found:
             while 1:
                 Directing()
-                print('Admin Page\n')
+                SetCo('green')
+                table('Admin Page\n' , 50 , 30)
+                table('' , 50 , 30)
+                print()
+                PutSp(30,' ')
                 print('choose: ')
-                print('1- Control Teacher')
-                print('2- Control Student')
-                print('0- Log out')
+                table('1- Control Teacher',20)
+                table('2- Control Student',20)
+                SetCo('red')
+                table('0- Log out',20)
+                table("",20)
+                PutSp(30,' ')
+                SetCo('gold')
                 Admin_choice = input('Choice: ')
-
+                
                 if Admin_choice == '0':
                     Returning()
                     return
@@ -46,10 +66,15 @@ def admin():
                     ctrl_student()
                 
                 else:
+                    SetCo('red')
                     print('Choose a number between 0 and 2')
+                    sound("Choose a number between 0 and 2")
                     press_any()
    
         else:
+            PutSp(30,' ')
+            SetCo('red')
             print('Id or Password is wrond please try again!!!')
-            press_any()
+            sound("yooooohhhh Id or Password is wrond please try again")
+            press_any()        
             Returning()
